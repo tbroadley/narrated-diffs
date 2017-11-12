563b473f8d50e48d8656e1a79dc6754a1f6d95ba
Thomas Broadley <buriedunderbooks@hotmail.com>
2017-11-12 10:24:23 -0500

    Show modified file line number

--- src/components/Change/Change.js
+++ src/components/Change/Change.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './Change.css'
 
-export default ({ type, ln1, ln, content }) => (
+export default ({ type, ln2, ln, content }) => (
   <div className={`change ${type === 'add' ? 'change--added' : ''} ${type === 'del' ? 'change--deleted' : ''}`}>
     <div className='change__addition-or-deletion'>
       {type === 'add' ? '+' : ''}
       {type === 'del' ? '-' : ''}
     </div>
     <div className='change__line-number'>
-      {type === 'normal' ? ln1 : ln}
+      {type === 'normal' ? ln2 : ln}
     </div>
     <div className='change__content'>
       {type === 'normal' ? content : content.slice(1)}
