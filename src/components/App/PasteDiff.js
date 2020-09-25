d87e445ad76bdbe9be09357116c0db86af943102
Thomas Broadley <buriedunderbooks@hotmail.com>
2020-09-25 18:05:45 -0400

    Add loading state to and beautify landing page

--- src/components/App/PasteDiff.js
+++ src/components/App/PasteDiff.js
@@ -5,7 +5,7 @@ import './PasteDiff.css'
 const { REACT_APP_SERVER_URL } = process.env;
 
 export class PasteDiff extends Component {
-  state = { tab: "PR", diff: "", url: "" };
+  state = { tab: "PR", diff: "", url: "", loading: false };
 
   onChange = (event) => {
     this.setState({ [event.target.name]: event.target.value });
@@ -16,16 +16,26 @@ export class PasteDiff extends Component {
   }
 
   fetchDiff = async () => {
+    this.setState({ loading: true })
     const response = await fetch(`${REACT_APP_SERVER_URL}/github-diff?url=${encodeURIComponent(this.state.url.replace(/(\/pull\/\d+).*/, "$1.diff"))}`);
     this.props.setDiff(await response.text());
+    this.setState({ loading: false })
   };
 
   render() {
     return (
       <div className='paste-diff'>
         <div className='paste-diff__tabs'>
-          <div className='paste-diff__tab' onClick={this.onClickTab('PR')}>PR</div>
-          <div className='paste-diff__tab' onClick={this.onClickTab('DIFF')}>Diff</div>
+          <div
+            className={`paste-diff__tab ${this.state.tab === 'PR' ? 'paste-diff__tab--active': ''}`}
+            onClick={this.onClickTab('PR')}>
+              PR
+          </div>
+          <div
+            className={`paste-diff__tab ${this.state.tab === 'DIFF' ? 'paste-diff__tab--active': ''}`}
+            onClick={this.onClickTab('DIFF')}>
+              Diff
+          </div>
         </div>
 
         {this.state.tab === 'DIFF' && (
@@ -34,7 +44,15 @@ export class PasteDiff extends Component {
             <p>
               <textarea name="diff" value={this.state.diff} onChange={this.onChange} />
             </p>
-            <button onClick={() => this.props.setDiff(this.state.diff)}>Narrate that diff!</button>
+            <button onClick={() => {
+              this.setState({ loading: true })
+              this.props.setDiff(this.state.diff)
+            }}>
+              Narrate that diff!
+            </button>
+            {this.state.loading && (
+              <p>Loading...</p>
+            )}
           </div>
         )}
 
@@ -45,6 +63,9 @@ export class PasteDiff extends Component {
               <input name="url" value={this.state.url} onChange={this.onChange} />
             </p>
             <button onClick={() => this.fetchDiff()}>Narrate that diff!</button>
+            {this.state.loading && (
+              <p>Loading...</p>
+            )}
           </div>
         )}
       </div>
