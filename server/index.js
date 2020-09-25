b0ff8e436b6b46683e60e6f157cb0ef25b509d27
Thomas Broadley <buriedunderbooks@hotmail.com>
2020-09-25 10:26:23 -0400

    Add server skeleton

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import Hapi from '@hapi/hapi';
+
+const init = async () => {
+    const server = Hapi.server({
+        port: 3500,
+        host: 'localhost'
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
+});
+
+init();
