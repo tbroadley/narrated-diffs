eb2e6af4b6e460a3b9d7ccbefeb528bcec71c860
Thomas Broadley <buriedunderbooks@hotmail.com>
2020-09-25 10:32:36 -0400

    Add endpoint for fetching diff

--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
 import Hapi from '@hapi/hapi';
+import fetch from 'node-fetch'
 
 const init = async () => {
     const server = Hapi.server({
         port: 3500,
         host: 'localhost'
+    });
+
+    server.route({
+      method: 'GET',
+      path: '/diff',
+      handler: async (request, h) => {
+          const response = await fetch(request.query.url);
+          return await response.text();
+      }
     });
 
     await server.start();
